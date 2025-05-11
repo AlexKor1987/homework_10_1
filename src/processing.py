2414ac007129b77b8f2ba098d571772e58c5d082
Alexandr <AlexKor1987@gmail.com>
2025-05-11 16:42:12 +0500

    first_second_funct

--- src/processing.py
+++ src/processing.py
@@ -1,17 +1,28 @@
-'''В модуле processing напишите функцию filter_by_state, которая принимает список словарей и опционально значение для ключа
-state (по умолчанию 'EXECUTED'). Функция возвращает новый список словарей, содержащий только те словари, у которых ключ
-state соответствует указанному значению.'''
 from typing import Dict, List
+
 
 def filter_by_state(list_of_id: list[Dict],
                     state: str = "EXECUTED") -> list[Dict]:
+    '''Функция, которая принимает список словарей и опционально значение для ключа
+state (по умолчанию 'EXECUTED'). Функция возвращает новый список словарей, содержащий
+только те словари, у которых ключ state соответствует указанному значению.'''
     new_list_of_id = []
     for number in range(len(list_of_id)):
         if list_of_id[number]["state"] == state:
             new_list_of_id.append(list_of_id[number])
-        # print(list_of_id[number])
-    #print(new_list_of_id)
     return new_list_of_id
+
+
+def sort_by_date(list_of_id: list[Dict],
+                 reverse_date: bool = True) -> list[Dict]:
+    '''Функция, которая принимает список словарей и необязательный параметр, задающий
+    порядок сортировки(по умолчанию — убывание).Функция должна возвращать новый список,
+    отсортированный по дате(date).'''
+    sorted_by_date = []
+    sorted_by_date = sorted(list_of_id, key=lambda list_of_id: list_of_id["date"], reverse = reverse_date)
+    return sorted_by_date
+
+
 if __name__ == "__main__":
     dict_data = [
         {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
@@ -20,11 +31,4 @@ if __name__ == "__main__":
         {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
     ]
     print(filter_by_state((dict_data), state = "CANCELED"))
-
-
-
-
-'''
-В том же модуле напишите функцию sort_by_date, которая принимает список словарей и необязательный параметр, задающий
-порядок сортировки(по умолчанию — убывание).Функция должна возвращать новый список, отсортированный по дате(date).
-'''
+    print(sort_by_date((dict_data), reverse_date = False))
