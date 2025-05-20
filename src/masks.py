4635cdc2a8d8a90967672f28833c8b43c8d3d01a
Alexandr <AlexKor1987@gmail.com>
2025-05-20 23:21:33 +0500

    homework_10_2_ver_3_except_widget

--- src/masks.py
+++ src/masks.py
@@ -1,7 +1,7 @@
 from typing import AnyStr
 
 
-def get_mask_card_number(card_number: int) -> str:
+def get_mask_card_number(card_number: str) -> str:
     """Функция маскировки номера банковской карты"""
     counter = 0
     result_card_number = ""
@@ -12,21 +12,15 @@ def get_mask_card_number(card_number: int) -> str:
             temp_card_number.append("*")
         else:
             temp_card_number.append(number)
-
     res_1 = temp_card_number[:4]
     res_2 = temp_card_number[4:8]
     res_3 = temp_card_number[8:12]
     res_4 = temp_card_number[12:16]
     result_card_number = "".join(res_1) + " " + "".join(res_2) + " " + "".join(res_3) + " " + "".join(res_4)
-
     return result_card_number
 
 
-if __name__ == "__main__":
-    print(get_mask_card_number(7000792289606361))
-
-
-def get_mask_account(account_number: int) -> str:
+def get_mask_account(account_number: str) -> str:
     """Функция маскировки номера банковского счета"""
     counter = 0
     temp_account_number = []
@@ -41,4 +35,6 @@ def get_mask_account(account_number: int) -> str:
 
 
 if __name__ == "__main__":
+    print(get_mask_card_number(7000792289606361))
     print("".join(get_mask_account(73654108430135874305)))
+
---
+++ tests/test_main.py
@@ -0,0 +1,37 @@
+import pytest
+
+from src.masks import get_mask_card_number, get_mask_account
+from src.processing import filter_by_state, sort_by_date
+#from src.widget import mask_account_card, get_date
+
+dict_data = [
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    ]
+
+
+def test_get_mask_card_number(card_numbers):
+    assert get_mask_card_number('7000792289606361') == card_numbers
+
+
+def test_get_mask_account(mask_accounts):
+    assert get_mask_account('73654108430135874305') == mask_accounts
+
+
+def test_filter_by_state(by_state):
+    assert filter_by_state((dict_data), state="CANCELED") == by_state
+
+
+def test_sort_by_date(by_date):
+    assert sort_by_date((dict_data), reverse_date=False) == by_date
+
+
+#def test_mask_account_card(account_cards):
+#    assert mask_account_card('Maestro 1596837868705199') == account_cards
+
+
+#def test_get_date(getting_date):
+#    assert get_date('2024-03-11T02:26:18.671407') == getting_date
+
