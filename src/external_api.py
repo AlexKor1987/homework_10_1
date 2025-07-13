f9a2defebacdd07901c62d03b961f4d88e69042d
Alexandr <AlexKor1987@gmail.com>
2025-07-14 01:40:51 +0500

    Homework_12_1_ver7_all_new__all_done_2

--- src/external_api.py
+++ src/external_api.py
@@ -1,61 +1,50 @@
-import os
-from dotenv import load_dotenv
-import requests
 import json
-from typing import Any, Dict, Generator, Iterator, List
+import os
+from typing import Dict, List
+
+import requests
+from dotenv import load_dotenv
+
 from src.utils import open_file_get_transactions
-from unittest.mock import Mock
-from unittest.mock import patch
-#from tests.test_generators import transactions
+
+EXCHANGE_RATES_API_KEY = os.getenv("API_KEY")
 
 
-def convert_to_rub(transaction):
-  """Функция конвертации валюты (принимает на вход транзакцию и возвращает сумму транзакуций
-  (amount) в рублях, тип данных "float". Если транзакция была а "USD" или "EUR" происходит
-  обращение к внешнему API"""
-  operation_amount = transaction.get("operationAmount", {})
-  amount = operation_amount.get("amount", 0)
-  currency_code = operation_amount.get("currency", {}).get("code", 0)
-  if currency_code == "RUB":
-    return amount
-  elif currency_code == "USD" or currency_code == "EUR":
-    load_dotenv()  # Загрузка переменных из .env-файла
-    apikey = os.getenv('API_KEY')  # Получение значения переменной API_KEY из .env-файла
-    url = f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from={currency_code}&amount={amount}"
-    headers = {
-      "apikey": apikey
-    }
-    try:
-      response = requests.get(url, headers=headers)
-      if response.status_code == 200:
-        return float(response.json()['result'])
-      else:
-        return None
-    except requests.exception.RequestException as error:
-      print ("Failed to get currency rate ", error)
-      return 0.0
+def convert_to_rub(transaction: dict) -> float:
+    """Функция конвертации валюты (принимает на вход транзакцию и возвращает сумму транзакуций
+    (amount) в рублях, тип данных "float". Если транзакция была а "USD" или "EUR" происходит
+    обращение к внешнему API"""
+    amount = transaction["operationAmount"]["amount"]
+    currency_code = transaction["operationAmount"]["currency"]["code"]
+    if currency_code == "RUB":
+        return float(amount)
+    url = "https://api.apilayer.com/exchangerates_data/convert"
+    params = {"to": "RUB", "from": currency_code, "amount": amount}
+    headers = {"apikey": EXCHANGE_RATES_API_KEY}
+    response = requests.get(url, params=params, headers=headers)
+    response.raise_for_status()
+    data = response.json()
+    return float(data["result"])
 
 
 if __name__ == "__main__":
-  transactions = open_file_get_transactions('C:/Users/PB/Desktop/Python/homework_10_1/data/operations.json')
-  for transaction in transactions:
-    convert_to_rub(transaction)
+    transactions = open_file_get_transactions(
+        "C:/Users/PB/Desktop/Python/homework_10_1/data/operations.json"
+    )
+    for transaction in transactions:
+        convert_to_rub(transaction)
 
+    # Для проверки без использования API
+    dict1 = {
+        "id": 587085106,
+        "state": "EXECUTED",
+        "date": "2018-03-23T10:45:06.972075",
+        "operationAmount": {
+            "amount": "48223.05",
+            "currency": {"name": "руб.", "code": "RUB"},
+        },
+        "description": "Открытие вклада",
+        "to": "Счет 41421565395219882431",
+    }
 
-# Для проверки без использования API
-  dict1 =   {
-     "id": 587085106,
-    "state": "EXECUTED",
-    "date": "2018-03-23T10:45:06.972075",
-    "operationAmount": {
-      "amount": "48223.05",
-      "currency": {
-        "name": "руб.",
-        "code": "RUB"
-      }
-    },
-    "description": "Открытие вклада",
-    "to": "Счет 41421565395219882431"
-  }
-
-  #print(convert_to_rub(dict1))
+# print(convert_to_rub(dict1))
--- src/utils.py
+++ src/utils.py
@@ -1,14 +1,15 @@
 import json
-from typing import List, Dict, Any
+from typing import Any, Dict, List
 from unittest.mock import patch
 
 
 def open_file_get_transactions(file_path: str) -> list[Dict[str, Any]]:
     """Функция открытия файла - принимает на вход путь до json - файла и
     возвращает список словарей с данными о финансовых транзакциях.
-    Если файл пустой, содержит не список или не найден, функция возвращает пустой список."""
+    Если файл пустой, содержит не список или не найден, функция возвращает пустой список.
+    """
     try:
-        with open(file_path, 'r', encoding="utf-8") as file:
+        with open(file_path, "r", encoding="utf-8") as file:
             data_json = json.load(file)
             if isinstance(data_json, list):
                 return data_json
@@ -17,4 +18,8 @@ def open_file_get_transactions(file_path: str) -> list[Dict[str, Any]]:
 
 
 if __name__ == "__main__":
-    print(open_file_get_transactions('C:/Users/PB/Desktop/Python/homework_10_1/data/operations.json'))
+    print(
+        open_file_get_transactions(
+            "C:/Users/PB/Desktop/Python/homework_10_1/data/operations.json"
+        )
+    )
