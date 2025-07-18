0b3918c94b95c274f87514b2b3740015e45dc9ca
Alexandr <AlexKor1987@gmail.com>
2025-07-18 17:10:11 +0500

    Homework_12_2_ver1_adjust_utils

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,22 @@
 import json
 from typing import Any, Dict, List
 from unittest.mock import patch
+import logging
+import os
+
+
+os.makedirs('logs', exist_ok=True)
+logger = logging.getLogger("utils")
+logger.setLevel(logging.DEBUG)
+
+file_handler = logging.FileHandler('logs/utils.log', mode='w', encoding='utf-8')
+file_handler.setLevel(logging.DEBUG)
+
+file_formatter = logging.Formatter('%(asctime)s %(levelname)s: $(message)s')
+file_handler.setFormatter(file_formatter)
+
+
+
 
 
 def open_file_get_transactions(file_path: str) -> list[Dict[str, Any]]:
@@ -9,11 +25,15 @@ def open_file_get_transactions(file_path: str) -> list[Dict[str, Any]]:
     Если файл пустой, содержит не список или не найден, функция возвращает пустой список.
     """
     try:
+        logger.debug(f"Попытка загрузить файл: {file_path}")
         with open(file_path, "r", encoding="utf-8") as file:
             data_json = json.load(file)
             if isinstance(data_json, list):
                 return data_json
-    except:
+    except FileNotFoundError:
+        return []
+    except Exception as e:
+        logger.error(f"Ошибка: {e}")
         return []
 
 
